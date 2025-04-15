c06b2b0117a1679b0c64a30c0ccc058242c04548
LOUDO <lucaslelouer@gmail.com>
2025-04-15 21:50:01 +0200

    Fixed: autre projet qui se montre pas sur mobile

--- script.js
+++ script.js
@@ -1,65 +1,74 @@
 const introMessage = new Typed(".word-change", {
-    strings: ["Développeur", "Passionné du", "Friand du", "Admirateur du", "Amoureux du", "Affectionne le", "Adore le", "Apprécie le"],
-    typeSpeed: 70,
-    backDelay: 1300,
-    startDelay: 450,
-    loop: true
-})
+  strings: [
+    "Développeur",
+    "Passionné du",
+    "Friand du",
+    "Admirateur du",
+    "Amoureux du",
+    "Affectionne le",
+    "Adore le",
+    "Apprécie le",
+  ],
+  typeSpeed: 70,
+  backDelay: 1300,
+  startDelay: 450,
+  loop: true,
+});
 
 const introDesc = new Typed(".pres-desc", {
-    strings: ["Un Étudiant de 19 ans qui souhaite travailler dans le web."],
-    typeSpeed: 20,
-    startDelay: 1500,
-    showCursor: false
-})
+  strings: ["Un Étudiant de 19 ans qui souhaite travailler dans le web."],
+  typeSpeed: 20,
+  startDelay: 1500,
+  showCursor: false,
+});
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        console.log(entry)
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
       if (entry.isIntersecting) {
-        if(entry.target.classList[0] == "projets-container"){
-            let projetCard = entry.target.children[entry.target.childElementCount-1].children;
-            for(let i = 0; i < projetCard.length; i++){
-                projetCard[i].classList.add('animation');
-            }
+        if (entry.target.classList[0] == "projets-container") {
+          let projetCard =
+            entry.target.children[entry.target.childElementCount - 1].children;
+          for (let i = 0; i < projetCard.length; i++) {
+            projetCard[i].classList.add("animation");
+          }
         } else {
-            entry.target.classList.add('animation');
+          entry.target.classList.add("animation");
         }
       }
     });
-  }, {
-	threshold: 0.25
-});
-  
+  },
+  {
+    threshold: 0.1,
+  }
+);
 
-const eleToAnim = 
-    [
-        document.querySelector('.me'), 
-        document.querySelector('.language p'), 
-        document.querySelector('.best-project') 
-    ]
-eleToAnim.forEach(element => {
-    observer.observe(element)
+const eleToAnim = [
+  document.querySelector(".me"),
+  document.querySelector(".language p"),
+  document.querySelector(".best-project"),
+];
+eleToAnim.forEach((element) => {
+  observer.observe(element);
 });
-
 
 let animDelay = 0.1;
 
-document.querySelectorAll(".logo").forEach(element => {
-    element.style.animationDelay = String(animDelay) + "s"
-    animDelay += 0.1
-    observer.observe(element)
+document.querySelectorAll(".logo").forEach((element) => {
+  element.style.animationDelay = String(animDelay) + "s";
+  animDelay += 0.1;
+  observer.observe(element);
 });
 
-animDelay = 0.1
+animDelay = 0.1;
 
-document.querySelectorAll(".projets-container").forEach(element => {
-    let projetCard = element.children[element.childElementCount-1].children
-    for(let i = 0; i < projetCard.length; i++){
-        projetCard[i].style.animationDelay = String(animDelay) + "s"
-        animDelay += 0.1
-    }
-    animDelay = 0.1;
-    observer.observe(element)
+document.querySelectorAll(".projets-container").forEach((element) => {
+  let projetCard = element.children[element.childElementCount - 1].children;
+  for (let i = 0; i < projetCard.length; i++) {
+    projetCard[i].style.animationDelay = String(animDelay) + "s";
+    animDelay += 0.1;
+  }
+  animDelay = 0.1;
+  observer.observe(element);
 });
-  
